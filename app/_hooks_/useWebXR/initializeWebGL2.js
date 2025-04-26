7c40db441a9396e39e93ed3a6c5439c4d19894ee
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-26 09:51:12 -0700

    remove three temporarily to make sure AR scene works properly first
    author: dangkyle64
    date: 4/26/2025

--- app/_hooks_/useWebXR/initializeWebGL2.js
+++ app/_hooks_/useWebXR/initializeWebGL2.js
@@ -11,18 +11,5 @@ export const initializeWebGl2 = (session) => {
     const xrLayer = new XRWebGLLayer(session, gl);
     session.updateRenderState({ baseLayer: xrLayer });
 
-    // Attach Three.js renderer to the WebGL context
-    renderer = new THREE.WebGLRenderer({ canvas, context: gl });
-    renderer.autoClear = false;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-
-    // Set renderer to use session
-    renderer.xr.setSession(session);
-
-    // Create a default scene and camera
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-
     return xrLayer;
 };
