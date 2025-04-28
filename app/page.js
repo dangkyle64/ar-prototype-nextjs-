2739e3854b125d747c2ce8d94d9fe9d6eb8716ca
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-27 20:57:19 -0700

    switch out the ARScene for the completed VideoButton for demo
    author: dangkyle64
    date: 4/27/2025

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import VideoButton from "./_components_/VideoButton";
 export default async function Home() {
     return (
       <div>
-            <h1>Hello World</h1>
-            
-            <ArScene />
+            <VideoButton />
       </div>
     );
 };
